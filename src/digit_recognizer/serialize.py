a72893edad99fec77b7989d1330461e7957ac184
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-03-04 22:20:09 -0500

    [#4] added init() to ExportModel class.
    Initializes variables in the ExportModel class.

--- src/digit_recognizer/serialize.py
+++ src/digit_recognizer/serialize.py
@@ -13,6 +13,8 @@ warnings.filterwarnings("ignore")
 logging.getLogger("tensorflow").setLevel(logging.FATAL)
 
 
+import tensorflow as tf
+
 from src.utils import create_log, add_to_log, set_physical_devices_memory_limit
 from src.digit_recognizer.train import Train
 
@@ -55,6 +57,28 @@ def serialize_model(model_version: str) -> None:
         trainer.model_configuration["model"]["final_image_width"],
         trainer.model_configuration["model"]["n_channels"],
     ]
+
+    class ExportModel(tf.Module):
+        """Exports a trained TensorFlow model as a TensorFlow module for serving."""
+
+        def __init__(self, model: tf.keras.Model) -> None:
+            """Initializes variables in the ExportModel class.
+
+            Initializes variables in the ExportModel class.
+
+            Args:
+                model: A tensorflow model with the latest trained checkpoint.
+
+            Returns:
+                None.
+            """
+            # Checks type & values of arguments.
+            assert isinstance(
+                model, tf.keras.Model
+            ), "Variable model should be of type 'tensorflow.keras.model'."
+
+            # Initializes class variables.
+            self.model = model
 
 
 def main():
