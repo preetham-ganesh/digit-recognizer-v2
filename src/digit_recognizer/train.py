c0c94a43c9b89d6c554bd74d4708c2bf55f14558
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-03-04 21:59:03 -0500

    [#3] added init() to Train class.
    Creates object attributes for the Train class.

---
+++ src/digit_recognizer/train.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import warnings
+import argparse
+import logging
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_PATH)
+warnings.filterwarnings("ignore")
+logging.getLogger("tensorflow").setLevel(logging.FATAL)
+
+
+class Train(object):
+    """Trains the digit recognition model based on the configuration."""
+
+    def __init__(self, model_version: str) -> None:
+        """Creates object attributes for the Train class.
+
+        Creates object attributes for the Train class.
+
+        Args:
+            model_version: A string for the version of the current model.
+
+        Returns:
+            None.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(model_version, str), "Variable model_version of type 'str'."
+
+        # Initalizes class variables.
+        self.model_version = model_version
+        self.best_validation_loss = None
