ff4f79254fcc8ee0ebe0635c321b5e24508f1fae
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-03-04 21:15:56 -0500

    [#2] added split_dataset() to Dataset class.
    Splits original train data into new train, validation & test data.

--- src/digit_recognizer/dataset.py
+++ src/digit_recognizer/dataset.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+from sklearn.utils import shuffle
 
 from src.utils import add_to_log
 
@@ -51,5 +52,58 @@ class Dataset(object):
             "No. of examples in the original train data: {}".format(
                 len(self.original_train_data)
             )
+        )
+        add_to_log("")
+
+    def split_dataset(self) -> None:
+        """Splits original train data into new train, validation & test data.
+
+        Splits original train data into new train, validation & test data.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # Computes number of examples in the train, validation and test datasets.
+        self.n_total_examples = len(self.original_train_data)
+        self.n_validation_examples = int(
+            self.model_configuration["dataset"]["split_percentage"]["validation"]
+            * self.n_total_examples
+        )
+        self.n_test_examples = int(
+            self.model_configuration["dataset"]["split_percentage"]["test"]
+            * self.n_total_examples
+        )
+        self.n_train_examples = (
+            self.n_total_examples - self.n_validation_examples - self.n_test_examples
+        )
+
+        # Shuffles the original train data.
+        self.original_train_data = shuffle(self.original_train_data, random_state=2)
+
+        # Splits the original train data into new train, validation & test data.
+        self.new_validation_data = self.original_train_data[
+            : self.n_validation_examples
+        ]
+        self.new_test_data = self.original_train_data[
+            self.n_validation_examples : self.n_test_examples
+            + self.n_validation_examples
+        ]
+        self.new_train_data = self.original_train_data[
+            self.n_test_examples + self.n_validation_examples :
+        ]
+
+        add_to_log(
+            "No. of examples in the new train data: {}".format(self.n_train_examples)
+        )
+        add_to_log(
+            "No. of examples in the new validation data: {}".format(
+                self.n_validation_examples
+            )
+        )
+        add_to_log(
+            "No. of examples in the new test data: {}".format(self.n_test_examples)
         )
         add_to_log("")
